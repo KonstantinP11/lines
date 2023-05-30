b63e745cee7cc56fcd8d11639e7af276aa0211ef
conditional <105pkn@mail.ru>
2023-05-30 22:30:04 +0700

    Домашка готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        System.out.println("Данные ФИО сотрудника для отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+        String fullNameSS = "Иванов Семён Семёнович";
+        String fixedFullName = fullNameSS.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fixedFullName);
     }
 }
